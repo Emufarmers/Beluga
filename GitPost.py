487cf783db65f196a501942365e8f70da1a5d9c6
Emalee <rs.of.chrs@gmail.com>
2012-01-06 22:41:49 -0800

    rm code that shouldn't be in gitpost

--- GitPost.py
+++ GitPost.py
@@ -54,9 +54,3 @@ class GitPost(BasicPlugin):
   def teardown(self):
     print dir(self.factory)
     self.listener.stopListening()
-  
-  def privmsg(self, user, channel, msg):
-    #This will get called when the bot receives a message.
-    if string.lower(self.IRC.nickname) in string.lower(msg):
-      self.IRC.me(channel, "snuggles %s" % user.split("!",1)[0])
-    return
