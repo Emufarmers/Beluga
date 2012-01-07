039c5ec84ff9d8ddbe8b147faebfc754f489b168
Emalee <rs.of.chrs@gmail.com>
2012-01-06 23:25:37 -0800

    say should use the factory channel

--- plugins/Say.py
+++ plugins/Say.py
@@ -9,5 +9,5 @@ class Say(BasicPlugin):
     if string.lower(self.IRC.nickname) in string.lower(channel):
       host = user.split('@',1)[1] if ('@' in user) else ""
       if host in NS_ADMINS:
-        self.IRC.msg(channel, msg)
+        self.IRC.msg(self.IRC.factory.channel, msg)
     return
